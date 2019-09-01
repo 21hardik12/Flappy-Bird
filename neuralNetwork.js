f05ea1f9e7e1a34845528ddf889524bd92bc19a5
21hardik12 <54760905+21hardik12@users.noreply.github.com>
2019-09-01 14:20:22 +0530

    Add files via upload

---
+++ neuralNetwork.js
@@ -0,0 +1,54 @@
+class NeuralNetwork {
+	constructor (n_input, n_hidden, n_output) {
+		this.sizes = [n_input, n_hidden, n_output];
+		this.weight_matrices = [];
+		this.bias_matrices = [];
+		
+		// filling up the weight matrices
+		for (let i = 0; i < this.sizes.length - 1; i++) {
+			this.weight_matrices[i] = new Matrix(this.sizes[i+1], this.sizes[i]);
+			this.weight_matrices[i].randomize();			
+		}
+		
+		// filling up the biase matrices
+		for (let i = 0; i < this.sizes.length - 1; i++) {
+			this.bias_matrices[i] = new Matrix(this.sizes[i+1], 1);
+			this.bias_matrices[i].randomize();			
+		}
+	}
+	
+	feedForward(input) {
+		input = Matrix.fromArray(input);
+		input.visualize();
+		for (let i = 0; i < this.sizes.length - 1; i++) {
+			input = Matrix.dot(this.weight_matrices[i], input);			
+			input.add(this.bias_matrices[i]);			
+			input.activate();
+		}		
+		
+		return input;
+	}
+	
+	copy() {
+		let clone = new NeuralNetwork(this.sizes[0], this.sizes[1], this.sizes[2]);
+		for (let i = 0; i < this.sizes.length - 1; i++) {
+			clone.weight_matrices[i] = this.weight_matrices[i].copy();
+			clone.bias_matrices[i] = this.bias_matrices[i].copy();
+		}
+	}
+	
+	crossover(partner) {
+		let child = new NeuralNetwork(this.sizes[0], this.sizes[1], this.sizes[2]);
+		for (let i = 0; i < this.sizes.length - 1; i++) {
+			child.weight_matrices[i] = this.weight_matrices[i].crossover(partner.weight_matrices[i]);
+			child.bias_matrices[i] = this.bias_matrices[i].crossver(partner.bias_matrices[i]);
+		}
+	}
+	
+	mutate(mutationRate) {
+		for (let i = 0; i < this.sizes.lenght - 1; i++) {
+			this.weight_matrices[i].mutate(mutationRate);
+			this.bias_matrices[i].mutate(mutationRate);
+		}
+	}
+}
